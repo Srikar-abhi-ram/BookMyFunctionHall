3253441e873a3262d73e1e8bfece0794ef19c6be
Srikar-abhi-ram <srikar@profintech.in>
2025-07-01 19:40:48 +0530

    Configured backend

---
+++ BackEnd/src/main/java/com/example/PelliPandiri/PelliPandiriApplication.java
@@ -0,0 +1,13 @@
+package com.example.PelliPandiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelliPandiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelliPandiriApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/example/PelliPandiri/PelliPandiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PelliPandiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelliPandiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
